b8d239fb07d079fc566fc40a6b64c85d4d6f79f8
BelascuDorin <Dorin.Belascu@gmail.com>
2021-01-05 10:37:44 +0200

    Feching and displaying logs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
+
+
 
 const App = () => {
   useEffect(() => {
@@ -10,9 +14,12 @@ const App = () => {
   });
 
   return (
-    <div className="App">
-      My App
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
   );
 }
 
